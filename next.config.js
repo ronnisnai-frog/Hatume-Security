d8f064b51f7ab26ed2789e6aa41977c19c8762eb
Ronnis Nai <ronnisnai123@gmail.com>
2026-09-03 08:53:21 +1000

    Add environment variable for build ID in config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BUILD_ID: process.env.VERCEL_GIT_COMMIT_SHA || "dev",
+  },
+};
 
 module.exports = nextConfig;
